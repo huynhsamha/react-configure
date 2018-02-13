4d618b40854b0ffe8c5e04a6490357f2742bc7c8
huynhsamha <huynhsamha@gmail.com>
2018-02-13 23:06:00 +0700

    do action for react-table

--- src/components/pages/demo-react-table/DemoReactTable.jsx
+++ src/components/pages/demo-react-table/DemoReactTable.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import * as Actions from './../../../actions';
+
 import { FormattedDate, FormattedRelative, FormattedPlural } from 'react-intl';
 
 import ReactTable from 'react-table';
@@ -11,7 +13,8 @@ import matchSorter from 'match-sorter';
 import './style.css';
 
 const propTypes = {
-  todos: PropTypes.array
+  todos: PropTypes.array,
+  removeTodo: PropTypes.func.isRequired
 };
 
 const defaultProps = {
@@ -19,6 +22,7 @@ const defaultProps = {
 };
 
 class DemoReactTable extends Component {
+
   constructor(props) {
     super(props);
 
@@ -27,6 +31,19 @@ class DemoReactTable extends Component {
     console.log('contructor');
 
     console.log(this.props.todos);
+  }
+
+  onClickRemoveTodo(rowInfo) {
+    console.log(rowInfo);
+    const data = rowInfo.row;
+    const res = window.confirm(`Do you want to remove todo ${data.id}`);
+    if (res) {
+      this.props.removeTodo(data.id);
+    }
+  }
+
+  onClickEditTodo(rowInfo) {
+    console.log(rowInfo);
   }
 
   render() {
@@ -53,7 +70,8 @@ class DemoReactTable extends Component {
       {
         Header: props => <span className="tb-header">Duration</span>,
         accessor: 'duration',
-        maxWidth: 100,
+        maxWidth: 85,
+        resizable: false,
         Cell: props => (
           <div className="tb-cell text-center">
             {props.value} <FormattedPlural value={props.value} one="day" other="days" />
@@ -142,6 +160,36 @@ class DemoReactTable extends Component {
             default: return false;
           }
         }
+      },
+      {
+        Header: '',
+        width: 30,
+        filterable: false,
+        sortable: false,
+        resizable: false,
+        id: 'btn-edit',
+        Cell: props => (
+          <div className="tb-cell text-center tb-buttons">
+            <span className="tb-button text-center cursorPointer text-success" role="button" tabIndex={0}>
+              <i className="fa fa-pencil-square-o" />
+            </span>
+          </div>
+        )
+      },
+      {
+        Header: '',
+        width: 30,
+        filterable: false,
+        sortable: false,
+        resizable: false,
+        id: 'btn-remove',
+        Cell: props => (
+          <div className="tb-cell text-center tb-buttons">
+            <span className="tb-button text-center cursorPointer text-danger" role="button" tabIndex={0}>
+              <i className="fa fa-trash-o" />
+            </span>
+          </div>
+        )
       }
     ];
 
@@ -164,6 +212,17 @@ class DemoReactTable extends Component {
           }}
           noDataText="No anything found"
           className="-striped -highlight react-table"
+          getTdProps={(state, rowInfo, column) => ({
+            onClick: (e) => {
+              console.log(state);
+              console.log(column);
+              console.log(rowInfo);
+              switch (column.id) {
+                case 'btn-remove': return this.onClickRemoveTodo(rowInfo);
+                case 'btn-edit': return this.onClickEditTodo(rowInfo);
+              }
+            }
+          })}
         />
       </div>
     );
@@ -178,7 +237,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  removeTodo: id => dispatch(Actions.removeTodo(id))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(DemoReactTable);
